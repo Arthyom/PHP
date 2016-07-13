8f0b5c3f8df732a2b35814898bded1ce943196dc
arthyom <allpipiasaaa@hotmail.com>
2016-07-12 19:44:35 -0500

    Hola php

---
+++ php1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Pagina 1 de phpa</title>
+</head>
+<body>
+	<?php
+
+		// usando el php por primera vez 
+		echo "hola php";
+
+	?>
+
+</body>
+</html>
---
+++ php2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<?php
+		// variables 
+		/* 
+		las variables en php son de tipado dinamico, por lo que no necesitan definir tipos de dato para su uso, se declaran con el caracter $ 
+		*/
+
+		// declarar 3 variables para probrar las capacidades del php
+		$var1 = 1;
+		$var2 = "variable";
+		$var3 = TRUE;
+
+		echo "<h1> el encavezado 1 es $var1 </h1>" ;
+		echo "<h2> El encavezado 2 es $var2 </h2>";
+		echo "<h3> el encavezado 3 es $var3 </h3>";
+
+	?>
+
+</body>
+</html>
