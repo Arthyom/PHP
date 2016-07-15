ed91db5d1d88cd5ee1bc071f38ce5c2200baf64d
arthyom <allpipiasaaa@hotmail.com>
2016-07-14 20:38:18 -0500

    usando vectores asociativos

--- php9.php
+++ php9.php
@@ -8,14 +8,18 @@
 		 //  funciones de cadena variables 
 		/*** str_replacer ***/
 		// reemplaza una cadena indicada con otra dentro de una variable original
-		$varOriginal = "Cadena original";
+		$varOriginal = "cadena original en cuestion de cadena";
 
 		echo "$varOriginal<br/>";
 		echo str_replace("original", "nueva", $varOriginal), "<br/>";
 
 		// funciones de mayusculas y minusculas
 		echo strtoupper($varOriginal), " ";
-		echo strtolower($varOriginal), " ";
+		echo strtolower($varOriginal), "<br/>";
+
+		// funciones de repeticion y conteo
+		echo count_chars( $varOriginal, 'c');
+		echo substr_count( $varOriginal, "cadena" );
 	?>
 
 </body>
