cca8b9f8633f5755b34a67edd507fee61c0297e5
root <root@localhost.localdomain>
2016-07-21 16:36:11 -0500

    ejemplos del metodo get

---
+++ GetPHP1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Nombre y Datos PHP</title>
+</head>
+<body>
+	<form>
+		<strong><label>Nombre</label></strong>
+		<input type="textbox" name="txtName"/>
+
+		<br>
+
+		<strong><label>Apellido</label></strong>
+		<input type="textbox" name="txtApellido"/>
+		
+		<br>
+
+		<strong><label>Carrera</label></strong>
+		<input type="textbox" name="txtCarrera"/>
+
+	</form>
+</body>
+</html>
